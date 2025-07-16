1b885125c693e18b93e9afe676543a7efa25630a
Vitalisn4 <ngamvitalisyuh@gmail.com>
2025-07-16 23:16:38 +0100

    feat(routes): Implemented RESTful routes for note resource

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,31 +1,19 @@
-import express from 'express';
-import { createNotes, deleteNotes, getAllNotes, updateNotes } from "../controllers/notesController.js";
-
+import express from "express";
+import {
+  createNote,
+  deleteNote,
+  getAllNotes,
+  getNoteById,
+  updateNote,
+} from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
-
-router.post("/", createNotes);
-
-router.put("/:id", updateNotes);
-
-router.delete("/:id", deleteNotes);
+router.get("/:id", getNoteById);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router;
 
-// app.get("/api/notes", (req, res) => {
-//     res.status(200).send("you got 5 notes");
-// });
-
-// app.post("/api/notes", (req, res) => {
-//     res.status(201).json({ message: "Note created successfully!" })
-// });
-
-// app.put("/api/notes/:id", (req, res) => {
-//     res.status(200).json({ message: "Note updated successfully!" })
-// });
-
-// app.delete("/api/notes/:id", (req, res) => {
-//     res.status(200).json({ message: "Note deleted successfully!" })
-// });
