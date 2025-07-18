499945a1b5c58586797a6c1bc605ea66e8fa1427
Vitalisn4 <ngamvitalisyuh@gmail.com>
2025-07-18 05:00:54 +0100

    Modified file for the uploading

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,6 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 
-
 // Configure Cloudinary using the environment variable
 cloudinary.config({
   secure: true, // This is set by default if CLOUDINARY_URL is present
